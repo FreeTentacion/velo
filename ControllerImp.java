3b57a6ecdd5fa755671e14ddd3131c0fc36ca670
Edward Walpole <32910379+ohdear3000@users.noreply.github.com>
2017-11-25 01:44:22 +0000

    Add files via upload

--- ControllerImp.java
+++ ControllerImp.java
@@ -18,11 +18,16 @@ public class ControllerImp implements Controller {
     
     private Mode mode;
     private Location currentLocation;
-    private int currentStep;
+    private int currentStage;
     private boolean browseDetails;
     private String browseDetailsTourId;
     private Tour t;
+    private Waypoint currentWaypoint;
     private ArrayList<Waypoint> waypoints;
+    private double waypointradius;
+    private double waypointSeparation;
+    private int legcounter = 0;
+    private boolean lastinstructioncreatewaypoint = false;
 
     private String startBanner(String messageName) {
         return  LS 
@@ -31,13 +36,19 @@ public class ControllerImp implements Controller {
                 + "-------------------------------------------------------------";
     }
     
-    public ControllerImp(double waypointRadius, double waypointSeparation) {
+    public ControllerImp(double waypointRadius, double waypointSeparation) { // What's the difference between these two again?
+        this.waypointradius = waypointRadius;
+        this.waypointSeparation = waypointSeparation;
     }
     
     public Location getcurrentlocation() {
         return currentLocation;
     }
-
+    
+    // Not sure if this is needed
+    public ArrayList<Waypoint> getWaypoints() { 
+        return waypoints;
+    }
     //--------------------------
     // Create tour mode
     //--------------------------
@@ -56,9 +67,11 @@ public class ControllerImp implements Controller {
         ModeEnum.setMode(Mode.CREATE);
         
         t  = new Tour(id, title, annotation);
-        currentStep = 0;
+        currentStage = 0;
         
-        return new Status.Error("unimplemented");
+        addLeg(annotation); // adds the first leg, with the annotation directing the user to the starting location
+        currentStage++;
+        return Status.OK;
     }
 
     @Override
@@ -69,21 +82,35 @@ public class ControllerImp implements Controller {
             return new Status.Error("Current Mode is Invalid for Adding Waypoint.");
         }
         
-       /* Waypoint wp = new Waypoint(annotation, currentLocation);
-        t.addWaypoint(wp.near(currentLocation));
-        */
-        
+        if (currentStage == 1) { // doesn't need to check if it's too close to the previous waypoint, because the previous waypoint doesn't exist.
+            t.addWaypoint(currentLocation, annotation); // the add waypoint method in tour needs to update the array list in this class
+            lastinstructioncreatewaypoint = true;
+            return Status.OK;
+        }
+        // the following if statements checks two waypoints aren't being added at the same stage
+        if (waypoints.size() == currentStage) {
+            return new Status.Error("Already have a waypoint for this stage");  // again assuming this then cuts the method off here          
+        }
         // the following code checks that the user is outside of the previous waypoint radius before creating a new one 
-        //ControllerImp
         
-        waypoints[currentStep];
-        Displacement disone = new Displacement(east, north);
-        if (disone.distance())
+        currentWaypoint = waypoints.get(currentStage); // current waypoint
+        // get current location, and find the distance between currentWaypoint's location and that of the current location
+        Location l = currentWaypoint.getLocation();
+        double wapnorth = l.getnorthing();
+        double wapeast = l.geteasting();
+        double cureast = currentLocation.geteasting();
+        double curnorth = currentLocation.getnorthing();
+        double separationDistance = Math.sqrt(((wapnorth-curnorth)*(wapnorth-curnorth))+((wapeast-cureast)*(wapeast-cureast)));
+        if (separationDistance < waypointSeparation ) {
+            return new Status.Error("Too close to insert a new Waypoint."); // would this method just cut here if this error occurs?
+        }
+        else {
+            t.addWaypoint(currentLocation, annotation);
             
-        // then call the method in the Tour class
-        
-        
-        return new Status.Error("unimplemented");
+            
+        }
+        lastinstructioncreatewaypoint = true;
+        return Status.OK;
     }
 
     @Override
@@ -93,8 +120,18 @@ public class ControllerImp implements Controller {
         if ( !ModeEnum.isCreate() ) {
             return new Status.Error("Current Mode is Invaild for Adding Leg");
         }
-                
-        return new Status.Error("unimplemented");
+        if (legcounter>=currentStage) {
+            return new Status.Error("There is already a leg for the respective waypoint");
+        }
+        
+        t.addLeg(annotation);
+        
+        currentStage++; // now on two
+        legcounter++; // legcounter on one
+        
+        // does there need to be code so that a new leg can only be added after a new waypoint?
+        lastinstructioncreatewaypoint = false;
+        return Status.OK;
     }
 
     @Override
@@ -106,9 +143,16 @@ public class ControllerImp implements Controller {
             return new Status.Error("Current Mode is Invalid for Ending Tour");
         }
         
-        ModeEnum.setMode(Mode.BROWSE);
-        
-        return new Status.Error("unimplemented");
+        if (lastinstructioncreatewaypoint) {
+            ModeEnum.setMode(Mode.BROWSE); // does this fulfill this "the app should return to the overview sub-mode of the browse mode."
+            // output chunk containing title, number of legs, number of waypoints (added so far)
+            Chunk c = new Chunk().new CreateHeader(t.getTitle(), legcounter, currentStage -1 ); // error here? Constructor for chunk?
+            c.toString(); // outputs the chunk, lord knows why peanut butter jelly called these chunks
+            return Status.OK;
+        }
+        else {
+            return new Status.Error("Tour creation cannot end on a leg");
+        }
     }
 
     //--------------------------
--- Waypoint.java
+++ Waypoint.java
@@ -3,18 +3,26 @@ package tourguide;
 public class Waypoint {
     
     private Annotation annotation;
+    private Location location;
     
     public Waypoint(Annotation annotation, Location location) {
         this.annotation = annotation;
+        // nothing about the location in the constructor?
+        this.location = location;
     }
     
     public Annotation getannotation() {
         return annotation;
     }
     
+    public Location getLocation() {
+        return location;
+    }
+    
     
     public boolean near(Location location) {
         
+        return true;
         
         
         
